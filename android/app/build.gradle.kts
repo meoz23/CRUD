7303047c625cf7405e024a4cd5fdab53dcc0c35f
meoz23 <laboumyz@gmail.com>
2025-04-07 21:12:33 -0400

    Firebase SDK (app-level)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
 }
 
 android {
